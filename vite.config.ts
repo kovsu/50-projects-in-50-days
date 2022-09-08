73ed72ef014b2517a829386335a6f376240931fd
v <2583695112@qq.com>
2022-09-08 13:53:09 +0800

    chore: vite config 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [vue()],
   resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "src"),
-    },
+    alias: [{ find: "~/", replacement: `${resolve(__dirname, "src")}/` }],
   },
 });
